b8ad251f7f151ea692b6d8c8c1f7f53922fe507c
einbulinda <einbulinda@gmail.com>
2021-04-13 09:48:43 +0300

    class basics

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+class User
+{
+    // properties and methods here.
+}
+// storing the new user in a variable => Instanciating a class
+$userOne = new User();
+// checking the class of the variable
+echo 'The class is ' . get_class($userOne);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OOP</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
